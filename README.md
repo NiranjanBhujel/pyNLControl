bb21b45c6e6fe29b9406351b559b7bf6d5472a26
Niranjan Bhujel <niranjan.bhujel2014@gmail.com>
2022-01-22 09:20:34 -0600

    Project started

---
+++ README.md
@@ -0,0 +1,18 @@
+# pyNLControl
+
+pyNLControl is the package for nonlinear control and estimation. It is python based package. Almost all of the module will be applicable to linear system as well. Some might be applicable only to nonlinear or linear only.
+
+## Requirements
+* python >= 3.6 (might work on older version of python3, not tested)
+* casadi>=3.5.5 and jinja2>=3.0.2
+
+## Installations
+
+```
+pip install pyNLControl
+```
+
+## Supported control and estimator
+* Estimators: Kalman filter, Extended Kalman Filter and Unscented Kalman Filter
+* Control: Nonlinear Model Predictive Control will be added soon
+* Misc: Nonlinear observability analysis, Noise covariance identification will be added soon
---
+++ setup.py
@@ -0,0 +1,30 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="pyNLControl",
+    version="0.0.1",
+    author="Niranjan Bhujel",
+    author_email="niranjan.bhujel2014@gmail.com",
+    description="Package for non-linear control and estimation.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/NiranjanBhujel/pyNLControl",
+    project_urls={
+        "Bug Tracker": "https://github.com/NiranjanBhujel/pyNLControl/issues",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src"),
+    python_requires=">=3.6",
+    install_requires=[
+        "casadi",
+        "jinja2",
+    ]
+)
